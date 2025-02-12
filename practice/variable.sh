f3a91c31ffd144b71aaedd90f9313c08524156a2
Anas <142112995+mansurianas@users.noreply.github.com>
2025-02-13 02:50:18 +0530

    Create  variable.sh

---
+++ practice/variable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+<<note
+
+this is a demo for variable
+note
+
+
+
+read -p "enter the name:" name
+
+echo "MY name is $name"
+
+
+
+read -p "what is your name?" name2
+
+echo "my name is $name2"
