c0a4bda6d3ffbba5a1f337c7a3ec40a51f22d6da
Anas <ubuntu@LAPTOP-7E5UNQJ4>
2025-02-20 15:38:53 +0530

    add sol

---
+++ challange/day6/broke_resto/solution.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Function to read and display the menu from menu.txt file
+function display_menu() {
+    echo "Welcome to the Restaurant!"
+    echo "Menu:"
+    local item_number=1
+while IFS= read -r line; do
+    name="$(echo "$line" | cut -d',' -f1)"
+    price="$(echo "$line" | cut -d',' -f2)"
+    echo "$item_number. $name - ₹$price"
+    ((item_number++))
+done < "menu.txt"
+}
+
+# Function to calculate the total bill
+function calculate_total_bill() {
+    local total=0
+    # Calculate the total bill based on the customer's order
+    for item_number in "${!order[@]}"; do
+        price=$(sed -n "${item_number}p" "menu.txt" | cut -d',' -f2)
+        quantity="${order[$item_number]}"
+        ((total += price * quantity))
+    done
+    echo "$total"    
+
+}
+
+# Function to handle invalid user input
+function handle_invalid_input() {
+    echo "Invalid input! Please enter a valid item number and quantity."
+}
+
+# Main script
+display_menu
+
+# Ask for the customer's name
+# TODO: Ask the customer for their name and store it in a variable "customer_name"
+
+echo -n "please enter customer's name: "
+read customer_name
+
+
+# Ask for the order
+echo "Please enter the item number and quantity (e.g., 1 2 for two Burgers):"
+read -a input_order
+
+# Process the customer's order
+declare -A order
+for (( i=0; i<${#input_order[@]}; i+=2 )); do
+    item_number="${input_order[i]}"
+    quantity="${input_order[i+1]}"
+
+
+    # TODO: Add the item number and quantity to the "order" array
+
+
+    if [[ $item_number =~ ^[0-9]+$ && $quantity =~ ^[0-9]+$ ]]; then
+        order["$item_number"]="$quantity"
+    else
+        handle_invalid_input
+        exit 1
+    fi
+
+done
+
+# Calculate the total bill
+total_bill=$(calculate_total_bill)
+
+# Display the total bill with a personalized thank-you message
+# TODO: Display a thank-you message to the customer along with the total bill
+# The message format: "Thank you, <customer_name>! Your total bill is ₹<total_bill>."
+echo "Thank you , $customer_name! Your total bill is   ₹$total_bill."
